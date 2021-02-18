dcd51744c1f11b73f146147c2339fd665326a354
Alexander Granhof <alexander.g737@gmail.com>
2021-02-18 16:03:36 +0100

    App component can now slide between components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,37 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useRef, useState } from 'react'
+import styles from './App.module.scss'
 
-function App() {
+const App = () => {
+    const [offset, setOffset] = useState(0)
+    const mainRef = useRef<HTMLElement>(null)
+
+    const createOffset = (multiplier = 1) => {
+        const pageWidth = document.body.clientWidth
+        const componentCount = mainRef.current?.childElementCount ?? 0
+        const maxOffsetLimit = -((componentCount - 1) * pageWidth) // We negate the number since going right lowers the number
+
+        const desiredOffset = offset + pageWidth * multiplier
+        const finalOffset = Math.max(Math.min(desiredOffset, 0), maxOffsetLimit)
+
+        setOffset(finalOffset)
+    }
+
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.tsx</code> and save to reload.
-                </p>
-                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <React.Fragment>
+            <span onClick={() => createOffset(1)} className={styles['left']}></span>
+            <span onClick={() => createOffset(-1)} className={styles['right']}></span>
+            <main ref={mainRef} style={{ transform: `translateX(${offset}px)` }}>
+                <article>
+                    <h1>Component 1</h1>
+                </article>
+                <article>
+                    <h1>Component 2</h1>
+                </article>
+                <article>
+                    <h1>Component 3</h1>
+                </article>
+            </main>
+        </React.Fragment>
     )
 }
 
